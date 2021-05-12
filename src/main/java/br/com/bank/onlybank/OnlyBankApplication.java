31af348b4be6dd7707f329e3735d4dd6e5197ff8
Audinéia Carmo <audineia.carmo@ilia.digital>
2021-05-11 23:20:52 -0300

    :recycle: Project Initial

---
+++ src/main/java/br/com/bank/onlybank/OnlyBankApplication.java
@@ -0,0 +1,13 @@
+package br.com.bank.onlybank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlyBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlyBankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/bank/onlybank/OnlyBankApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.bank.onlybank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlyBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
